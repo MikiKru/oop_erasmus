d9dd661960a79d5a2173fffef9588f8c9a6032c5
MikiKru <michal_kruczkowski@o2.pl>
2019-10-28 15:09:22 +0100

    oop v2

--- src/com/company/ex1/Ex1.java
+++ src/com/company/ex1/Ex1.java
@@ -1,6 +1,26 @@
 package com.company.ex1;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Ex1 {
-    private List<>
+    // specifier Type signature = initialization of new object
+    private List<User> users = new ArrayList<>();
+    // ------ microservices ------
+    // specifier returned_value signature([aguments]){}
 
+    // method for registration user in the users collection
+    public void addUser(String login, String password){
+        // create user - create new object of class User
+        User user = new User(login, password);
+        // save object user in users collection
+        users.add(user);
+    }
+    // method for presentation of users list
+    public void getAllUsers(){
+        // loops -> structure that enable iteratation of collections, table, etc
+        // for (Typ name (auxillary_object) : collection)
+        for (User u : users) {
+            System.out.println(u);
+        }
+    }
 }
